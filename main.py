f43c4ba95b302ecf5c6a2a437983bd86d8473e90
garlicfood1234 <garlicgarlic123456789@gmail.com>
2025-07-05 13:37:55 +0900

    #4 개발하는 동안 임시로 버전 3 판사 기능 쿨타임 제거

--- main.py
+++ main.py
@@ -6251,8 +6251,9 @@ async def judgement_(interaction: discord.Interaction, 시작: str, 끝: str = N
         # 쿨다운 확인
         if user_id not in bot.cooldowns:
             bot.cooldowns[user_id] = 0
-
-        if current_time - bot.cooldowns[user_id] < 1 * 60:  # 60초 = 1분
+        
+        if False : 
+        # if current_time - bot.cooldowns[user_id] < 1 * 60:  # 60초 = 1분
             embed = discord.Embed(
                 title=f"오류", # name
                 description=f"이 명령어는 1분마다 한 번 사용 가능합니다.",
