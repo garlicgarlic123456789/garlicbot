e8fc7c96706b0844785552b3c76b682cffa313cc
garlicfood1234 <garlicgarlic123456789@gmail.com>
2025-07-10 22:46:20 +0900

    유입경로 추가

--- main.py
+++ main.py
@@ -9820,6 +9820,7 @@ async def invite_log_check(link) :
     elif link == "QXNR5WJS9p" : return "TinyDev 연합 채널 홍보지"
     elif link == "kEbZeZTfTP" : return "하불의 믹서기 연구소 연합 채널 홍보지"
     elif link == "DhueXyrNUz" : return "무늬 서버 연합 채널 홍보지 또는 홍보 채널 홍보지"
+    elif link == "4T5pHkNqp" : return "나르님 서버 연합 채널 홍보지"
     else : return f"링크 {link}"
 
 @bot.tree.command(name = "유입경로확인", description = "유입경로를 확인합니다.")
