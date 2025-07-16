be4fa12d8e947d871172bc9f31821dbf38a2f504
garlicfood1234 <garlicgarlic123456789@gmail.com>
2025-07-16 21:45:56 +0900

    신림선이 노선: 1094로 표기되는 버그 수정

--- main.py
+++ main.py
@@ -9757,6 +9757,8 @@ class train_command(app_commands.Group) :
                 line = "서해선"
             elif arrival["subwayId"] == "1032 " : 
                 line = "GTX-A"
+            elif arrival["subwayId"] == "1094" : 
+                line = "신림선"
             else : 
                 line = arrival["subwayId"]  # 노선 ID (1001: 1호선, 1002: 2호선 등)
             direction = arrival["updnLine"]  # 상행/하행
