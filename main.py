021593bda038aea7499e31c5b330dbc0d310899d
garlicfood1234 <garlicgarlic123456789@gmail.com>
2025-07-22 13:18:17 +0900

    #61 d92efa7 에서도 해결 못 한 버그 결국 개발 포기 및 #61 포기

--- main.py
+++ main.py
@@ -2101,10 +2101,7 @@ async def on_raw_reaction_add(payload) :
 
     embed = discord.Embed(title="반응 추가됨", color=discord.Color.blue())
     embed.add_field(name="사용자", value=f"<@{payload.user_id}>", inline=True)
-    if not payload.emoji.is_unicode_emoji : 
-        embed.add_field(name="반응", value=f"<:{payload.emoji.name}:{payload.emoji.id}>", inline=True)
-    else : 
-        embed.add_field(name="반응", value=f":{payload.emoji.name}:", inline=True)
+    embed.add_field(name="반응", value=f":{payload.emoji.name}:", inline=True)
     embed.add_field(name="메시지 링크", value=message_link, inline=False)
     
     await channel.send(embed=embed)
