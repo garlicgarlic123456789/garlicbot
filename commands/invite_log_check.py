5158d00c2fc9c769cac8076457feac9df033a477
garlicfood1234 <garlicgarlic123456789@gmail.com>
2025-07-30 19:55:27 +0900

    유입경로추가

--- commands/invite_log_check.py
+++ commands/invite_log_check.py
@@ -58,4 +58,5 @@ async def invite_log_check(link) :
     elif link == "DhueXyrNUz" : return "무늬 서버 연합 채널 홍보지 또는 홍보 채널 홍보지"
     elif link == "24T5pHkNqp" : return "나르님 서버 연합 채널 홍보지"
     elif link == "4X4fnTCe6S" : return "전국단위 잡담방 홍보 채널 홍보지"
+    elif link == "hpGrRQKB9g" : return "KR Admin Fourm 홍보지"
     else : return f"링크 {link}"
